2ce6c51ff4b1a70708f78721b3d3e8ad8c75ed68
AdamMcChesney <adam.mcchesney@kbslp.com>
2017-01-13 12:25:48 -0600

    updated fixing test case

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-console.log("Hello World!")
-
 exports.sayHelloInEnglish = function() {
   return "Hello World";
 };
--- test/test.hello.js
+++ test/test.hello.js
@@ -1,15 +1,11 @@
-var index = require("../index.js" ),
-event = require("./event.json"),
-expect = require("chai").expect,
-context = null;
+var index = require("../index.js" );
+var expect = require("chai").expect;
 const assert = require('assert'); 
 
-describe("some function we are going to run", function () {
-
-    it("returns back the first key value", function() {
-        index.handler(event, context, function(err, res) {
-        //    assert(res === "value1")
-        expect(res === "value1").be.true
-        })
+describe("Hello world function", function () {
+    it("returns back hello world in english", function() {
+        console.log(index.sayHelloInEnglish());
+        //expect(index.sayHelloInEnglish === "Hello World").be.true
+        assert(index.sayHelloInEnglish() == "Hello World","Hello World expected")
     })
 });
